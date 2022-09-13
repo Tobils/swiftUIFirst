1678d1197847e6bc9df4f1d16c2076ddce606903
suhada <suhada@pts.sc>
2022-09-13 19:55:39 +0700

    Initial Commit

---
+++ Shared/SwiftLesson01App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLesson01App.swift
+//  Shared
+//
+//  Created by Ade Suhada on 13/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLesson01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
